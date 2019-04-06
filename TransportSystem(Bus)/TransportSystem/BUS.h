7a389e49fb66847ce54fb205fcd6f37e260ab2df
Nowshin Tabassum <nowshintabassum888@gmail.com>
2019-04-07 01:04:45 +0600

    BUS Section-Consists of All Bus Infos Also consists of an Admin Section which is password protected.The admin Can Enter this section and Update/Delete The bus Infos

---
+++ TransportSystem(Bus)/TransportSystem/BUS.h
@@ -0,0 +1,161 @@
+#pragma once
+#include"Admin.h"
+
+namespace TransportSystem {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+	using namespace MySql::Data::MySqlClient;
+	/// <summary>
+	/// Summary for BUS
+	/// </summary>
+	public ref class BUS : public System::Windows::Forms::Form
+	{
+	public:
+		BUS(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~BUS()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^  button1;
+	protected:
+	private: System::Windows::Forms::DataGridView^  dataGridView1;
+	private: System::Windows::Forms::BindingSource^  bindingSource1;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::ComponentModel::IContainer^  components;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = (gcnew System::ComponentModel::Container());
+			System::Windows::Forms::DataGridViewCellStyle^  dataGridViewCellStyle1 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
+			System::Windows::Forms::DataGridViewCellStyle^  dataGridViewCellStyle2 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(BUS::typeid));
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->bindingSource1 = (gcnew System::Windows::Forms::BindingSource(this->components));
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->bindingSource1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::Color::Peru;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Palatino Linotype", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->ForeColor = System::Drawing::SystemColors::Info;
+			this->button1->Location = System::Drawing::Point(521, 48);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(331, 67);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Fetch All Info";
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &BUS::button1_Click);
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->BackgroundColor = System::Drawing::SystemColors::ButtonHighlight;
+			this->dataGridView1->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			dataGridViewCellStyle1->Alignment = System::Windows::Forms::DataGridViewContentAlignment::MiddleLeft;
+			dataGridViewCellStyle1->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
+			dataGridViewCellStyle1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			dataGridViewCellStyle1->ForeColor = System::Drawing::SystemColors::Info;
+			dataGridViewCellStyle1->SelectionBackColor = System::Drawing::SystemColors::Highlight;
+			dataGridViewCellStyle1->SelectionForeColor = System::Drawing::SystemColors::HighlightText;
+			dataGridViewCellStyle1->WrapMode = System::Windows::Forms::DataGridViewTriState::True;
+			this->dataGridView1->ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			dataGridViewCellStyle2->Alignment = System::Windows::Forms::DataGridViewContentAlignment::MiddleLeft;
+			dataGridViewCellStyle2->BackColor = System::Drawing::SystemColors::InfoText;
+			dataGridViewCellStyle2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			dataGridViewCellStyle2->ForeColor = System::Drawing::SystemColors::Info;
+			dataGridViewCellStyle2->SelectionBackColor = System::Drawing::SystemColors::Highlight;
+			dataGridViewCellStyle2->SelectionForeColor = System::Drawing::SystemColors::HighlightText;
+			dataGridViewCellStyle2->WrapMode = System::Windows::Forms::DataGridViewTriState::False;
+			this->dataGridView1->DefaultCellStyle = dataGridViewCellStyle2;
+			this->dataGridView1->GridColor = System::Drawing::SystemColors::Control;
+			this->dataGridView1->Location = System::Drawing::Point(360, 192);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->Size = System::Drawing::Size(642, 282);
+			this->dataGridView1->TabIndex = 1;
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::Color::DarkGoldenrod;
+			this->button2->Font = (gcnew System::Drawing::Font(L"Modern No. 20", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button2->Location = System::Drawing::Point(521, 520);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(331, 89);
+			this->button2->TabIndex = 2;
+			this->button2->Text = L"ADMIN";
+			this->button2->UseVisualStyleBackColor = false;
+			this->button2->Click += gcnew System::EventHandler(this, &BUS::button2_Click);
+			// 
+			// BUS
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->ClientSize = System::Drawing::Size(1331, 646);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->dataGridView1);
+			this->Controls->Add(this->button1);
+			this->Name = L"BUS";
+			this->Text = L"BUS";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->bindingSource1))->EndInit();
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+		//fetch data
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+		String^ constr = "Server=127.0.0.1;Uid=root;pwd=;Database=student transport system";
+		MySqlConnection^ con = gcnew MySqlConnection(constr);
+		MySqlDataAdapter^ sda = gcnew MySqlDataAdapter("select * from bus", con);
+		DataTable^ dt = gcnew DataTable();
+		sda->Fill(dt);
+		bindingSource1->DataSource = dt;
+		dataGridView1->DataSource = bindingSource1;
+	}
+			 //admin
+	private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
+		this->Hide();
+		Admin^ f1 = gcnew Admin();
+		f1->ShowDialog();
+	}
+};
+}
